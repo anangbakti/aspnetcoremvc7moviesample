f14a419d912a62aae088bb0f26f3859697c788ac
anang shaleh bakti <anang.bakti@gmail.com>
2023-11-02 19:21:04 +0700

    Add view WelcomeViewData

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -6,8 +6,12 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET : /HelloWorld
-        public string Index() {
+/*        public string Index() {
             return "This is my default action...";
+        }*/
+
+        public IActionResult Index() { 
+            return View();
         }
 
         // GET : /HelloWorld/Welcome/
@@ -18,8 +22,15 @@ namespace MvcMovie.Controllers
 
         // GET : /HelloWorld/WelcomePerson/ with 2 params : name and numTimes
         // Requires using System.Text.Encodings.Web;
-        public string WelcomePerson(string name, int numTimes) {
+        public string WelcomePerson(string name, int numTimes=1) {
             return HtmlEncoder.Default.Encode($"Hello {name}, numTimes is {numTimes}");
         }
+
+        public IActionResult WelcomeViewData(string name, int numTimes = 1) {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;       
+            return View();
+        }
+
     }
 }
