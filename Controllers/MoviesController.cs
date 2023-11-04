41395e672402a35d88edd2de4a965fbed59d5110
anang shaleh bakti <anang.bakti@gmail.com>
2023-11-04 20:21:31 +0700

    Add search movies method get

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -20,11 +20,25 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-              return _context.Movie != null ? 
-                          View(await _context.Movie.ToListAsync()) :
-                          Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            if (_context.Movie == null) {
+                return Problem("Entity set 'MvcMovieContext.Movie' is null.");
+            }
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString)) {
+                movies = movies.Where(s => s.Title!.Contains(searchString));
+            }
+
+            return View(await movies.ToListAsync());
+        }
+
+        [HttpPost]
+        public string Index(string searchString, bool notUsed) {
+            return "From [HttpPost] Index : Filter on " +  searchString;
         }
 
         // GET: Movies/Details/5
